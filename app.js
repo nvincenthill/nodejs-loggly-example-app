4d336df51ec40ce715e60b706a24c50353c60f4b
mypersonalgithubaccount <nvincenthill@gmail.com>
2019-11-03 13:50:58 -0800

    Add Loggly logging

--- app.js
+++ app.js
@@ -6,40 +6,54 @@ const winston = require('winston'),
 
 require('winston-loggly-bulk');
 
+const { getRandomInt } = require('./helpers');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-const winstonConsoleTransport = new winston.transports.Console({
-  json: true,
-  colorize: true
-});
-
-const logglyTransport = new winston.transports.Loggly({
-  subdomain: process.env.SUBDOMAIN,
-  inputToken: process.env.CUSTOMER_TOKEN,
-  tags: ['Winston-NodeJS'],
-  json: true
-});
-
-// Place the express-winston logger before the router.
 app.use(
   expressWinston.logger({
-    transports: [logglyTransport]
+    transports: [
+      new winston.transports.Console({
+        json: true,
+        colorize: true
+      }),
+      new winston.transports.Loggly({
+        subdomain: process.env.SUBDOMAIN,
+        inputToken: process.env.CUSTOMER_TOKEN,
+        json: true,
+        tags: ['NodeJS']
+      })
+    ]
   })
 );
 
 app.get('/', (req, res) => {
-  throw 'This is an example error';
-  res.send('Hello World!');
+  const responseType = getRandomInt(2, 6);
+
+  switch (responseType) {
+    case 2:
+      res.send(200);
+      break;
+    case 3:
+      res.send(301);
+      break;
+    case 4:
+      res.send(400);
+      break;
+    case 5:
+      res.send(500);
+      break;
+    default:
+      throw 'This is a unhandled server error';
+  }
 });
 
+// log errors to the console and Loggly
 app.use(
   expressWinston.errorLogger({
     transports: [winstonConsoleTransport, logglyTransport],
-    format: winston.format.combine(
-      winston.format.colorize(),
-      winston.format.json()
-    )
+    format: winston.format.json()
   })
 );
 
